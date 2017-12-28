a6b4d14a605ac32158f42da02f65c00f80bb30da
Cameron <cam.mac@gmx.com>
2017-12-28 08:32:14 -0500

    added rotate in place
    man that was difficult to figure out. I’m sure there’s a more elegant
    way to do the rotate in place.

--- python/1-7.py
+++ python/1-7.py
@@ -7,7 +7,34 @@
 #   i.e. [[1,2],[3,4]] -> [[3,4],[1,2]]
 #2. zip consumes 1 item from the beginning of each argument and makes a tuple out of it until there are no more items
 def rotate(my2dArray):
-    return zip(*my2darray[::-1])
+    return zip(*my2dArray[::-1])
 
 
 #Bonus: Rotate in place
+def rotate(arr):
+    pretprint(arr)
+    print("\n")
+    mlength = len(arr)
+    N = mlength-1
+    if mlength == 2:
+        JL = 1
+    else:
+        JL = mlength//2
+    for j in range(JL):
+        for i in range(N):
+            temp = arr[N-i+j][j]
+            arr[N-i+j][j] = arr[N+j][N-i+j]
+            arr[N+j][N-i+j] = arr[i+j][N+j]
+            arr[i+j][N+j] = arr[j][i+j]
+            arr[j][i+j] = temp
+        N = N-2
+    return arr
+
+##Helper function, prettyprint the array
+def pretprint(mythingy):
+    for row in mythingy:
+        for el in row:
+            print("{} ".format(el), end='')
+        print("\r")
+
+pretprint(rotate([[1,2,3,4,5],[6,7,8,9,0],['A','B','C','D','E'],['F','G','H','I','J'],['K','L','M','N','O']]))
