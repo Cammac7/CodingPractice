f9b47796a1b067a52be1c24f71ba704321a9ae5e
Cameron <cam.mac@gmx.com>
2018-02-25 22:09:49 -0500

    pushed 17-10

--- python/chapter17/17-10.py
+++ python/chapter17/17-10.py
@@ -18,3 +18,33 @@ def majorityElement(inAr):
     return -1
 
 print(majorityElement(myArray))
+
+#Can it be done in O(N) time and O(1) space? Yes!
+
+def getCandidate(inArr):
+    majority = 0
+    count = 0
+    for n in inArr:
+        if count == 0:
+            majority = n
+        if n == majority:
+            count += 1
+        else:
+            count -=1
+    return majority
+
+def validate(inArr, majority):
+    count = 0
+    for j in inArr:
+        if j == majority:
+            count += 1
+    if count > len(inArr)//2:
+        return majority
+    return -1
+
+def spaceMajEle(inAr):
+    candidate = getCandidate(inAr)
+    if validate(inAr, candidate) != -1:
+        return candidate
+    return -1
+print(spaceMajEle(myArray))
